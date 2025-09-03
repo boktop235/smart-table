5351fa5b6e4ae23737ab4f2fc62062a762fbd779
boktop235 <nikitabokov235@mail.ru>
2025-09-03 11:11:44 +0300

    first commit

--- src/components/searching.js
+++ src/components/searching.js
@@ -1,11 +1,15 @@
 import {rules, createComparison} from "../lib/compare.js";
 
-
 export function initSearching(searchField) {
     // @todo: #5.1 — настроить компаратор
+    const searchRules = [
+        rules.skipEmptyTargetValues, // пропускаем пустые значения в цели сравнения
+        rules.searchMultipleFields(searchField, ['date', 'customer', 'seller'], false) // поиск по нескольким полям
+    ];
+    const compare = createComparison(searchRules);
 
     return (data, state, action) => {
         // @todo: #5.2 — применить компаратор
-        return data;
+        return data.filter(row => compare(row, state));
     }
 }
