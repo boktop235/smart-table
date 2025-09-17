54474f18fb8922c589660bac359051e4c38af5cc
boktop235 <nikitabokov235@mail.ru>
2025-09-17 17:16:16 +0300

    api update

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,35 +1,49 @@
-import {createComparison, defaultRules} from "../lib/compare.js";
-
-// @todo: #4.3 — настроить компаратор
-const compare = createComparison(defaultRules);
-
 export function initFiltering(elements, indexes) {
-    // @todo: #4.1 — заполнить выпадающие списки опциями
+  const updateIndexes = (elements, indexes) => {
     Object.keys(indexes).forEach((elementName) => {
-        elements[elementName].append(
-            ...Object.values(indexes[elementName]).map(name => {
-                const option = document.createElement('option');
-                option.value = name;
-                option.textContent = name;
-                return option;
-            })
-        );
+      elements[elementName].append(
+        ...Object.values(indexes[elementName]).map((name) => {
+          const el = document.createElement("option");
+          el.textContent = name;
+          el.value = name;
+          return el;
+        })
+      );
+    });
+  };
+
+  const applyFiltering = (query, state, action) => {
+    if (action?.name === "clear") {
+      const filterWrapper = action.closest(".filter-wrapper");
+      const input = filterWrapper?.querySelector("select, input");
+      if (input) {
+        input.value = "";
+        const field = input.name;
+        if (field) {
+          state[field] = "";
+        }
+      }
+    }
+
+    const filter = {};
+    Object.keys(elements).forEach((key) => {
+      if (elements[key]) {
+        if (
+          ["INPUT", "SELECT"].includes(elements[key].tagName) &&
+          elements[key].value
+        ) {
+          filter[`filter[${elements[key].name}]`] = elements[key].value; 
+        }
+      }
     });
 
-    return (data, state, action) => {
-        // @todo: #4.2 — обработать очистку поля
-        if (action && action.name === 'clear') {
-            const field = action.dataset.field;
-            const parent = action.closest('.filter-field');
-            const input = parent.querySelector('input, select');
-            
-            if (input) {
-                input.value = '';
-                state[field] = '';
-            }
-        }
-
-        // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data.filter(row => compare(row, state));
-    }
+    return Object.keys(filter).length
+      ? Object.assign({}, query, filter)
+      : query;
+  };
+  
+  return {
+    updateIndexes,
+    applyFiltering,
+  };
 }
